722db950146c05d9524d08dc4eb0aee6580f3f68
Darya Jenkins <melissegreenleaf@gmail.com>
2025-03-01 20:19:36 +0100

    edited

--- src/test/java/ru/netology/aqa/delivery/DataGenerator.java
+++ src/test/java/ru/netology/aqa/delivery/DataGenerator.java
@@ -5,16 +5,18 @@ import com.github.javafaker.Faker;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.util.Random;
 
 public class DataGenerator {
     protected static Faker faker = new Faker(new Locale("ru"));
+    protected static String[] cities = {"Москва", "Санкт-Петербург", "Казань", "Новосибирск", "Нижний Новгород"};
 
     public static String generateDate(int days, String pattern) {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
     }
 
     public static String generateCity() {
-        return faker.address().cityName();
+        return cities[new Random().nextInt(cities.length)];
     }
 
     public static String generateFullName() {
